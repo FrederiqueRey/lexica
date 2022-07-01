06d7f6fc9876032d1a850c53d82f2e02e2182499
JS Frédérique Rey <jsrey@wanadoo.fr>
2022-07-01 21:33:44 +0200

    Frontend

---
+++ frontend/lidellscott/src/components/SearchWord.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  data() {
+    return {
+      text: ''
+    }
+  },
+
+  mounted() {
+    this.axios
+      .get("http://localhost:8000/word/mgl/pater")
+      .then((response) => {
+        this.info = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  },
+
+  methods: {
+    onInput(e) {
+      this.text = e.target.value
+    }
+  }
+  
+}
+</script>
+
+<template>
+    <input :value="text" @input="onInput" placeholder="Search a Greek Word">
+    <p>{{ text }}</p>
+    <p>{{ info }}</p>
+
+</template>
